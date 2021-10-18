637c307e77930a96a82c431265fbc12cb8bb61ef
mehedirana <mdmehedirana@gmail.com>
2021-10-18 23:38:11 +0600

    resgistration post req added

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,20 +1,39 @@
+import axios from "axios";
 import React, { useState } from "react";
 import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
 import CommonBtn from "../common/CommonButton";
 import CommonInput from "../common/CommonInput";
 
-const RegisterScreen =({ navigation })=> {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [isFocus, setIsFocus] = useState(false);
+const url = 'http://apptest.dokandemo.com/wp-json/wp/v2/users/register';
+
+const RegisterScreen = ({ navigation }) => {
+  const [name, setName] = useState("mehedi");
+  const [email, setEmail] = useState("mehedi@gmail.com");
+  const [password, setPassword] = useState("123456");
+  const [isFocus, setIsFocus] = useState(false);
+
+  const handleRegister = async () => {
+    try {
+      
+      const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'JWT fefege...'
+      }
+      const res = await axios.post(url, {params : {
+        username: name,
+        email: email,
+        password: password
   
-    const handleLogin =()=>{
-      navigation.navigate('Home')
+      }}, headers )
+    console.log(res);
+    } catch (error) {
+      console.log(error);
     }
-    return (
-        <SafeAreaView
-      style={{ flex: 1,  backgroundColor: "#FFF", alignItems:'center', justifyContent:'center' }}
+    
+  }
+  return (
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: "#FFF", alignItems: 'center', justifyContent: 'center' }}
     >
       <CommonInput
         placeholder="User Name"
@@ -24,7 +43,7 @@ const RegisterScreen =({ navigation })=> {
         iname="lock"
         value={name}
         onChangeText={(t) => setName(t)}
-        onFocus={(t)=> setIsFocus(t)}
+        onFocus={(t) => setIsFocus(t)}
       />
       <CommonInput
         placeholder="User Email"
@@ -34,7 +53,7 @@ const RegisterScreen =({ navigation })=> {
         iname="phone"
         value={email}
         onChangeText={(t) => setEmail(t)}
-        onFocus={(t)=> setIsFocus(t)}
+        onFocus={(t) => setIsFocus(t)}
       />
       <CommonInput
         placeholder="User Password"
@@ -44,25 +63,25 @@ const RegisterScreen =({ navigation })=> {
         iname="lock"
         value={password}
         onChangeText={(t) => setPassword(t)}
-        onFocus={(t)=> setIsFocus(t)}
+        onFocus={(t) => setIsFocus(t)}
       />
 
       <CommonBtn
         title='Confirm'
         backColor="#004dcf"
         titleColor='#FFF'
-        onPress={handleLogin}
+        onPress={handleRegister}
       />
-      <View style={{flexDirection:'row'}}>
-       
-          <Text>Already Registered ? </Text>
-          <TouchableOpacity  onPress={()=> navigation.navigate('Login')}>
-            <Text style={{color:'#004dcf' , fontWeight:'bold'}}>Log in</Text>  
-          </TouchableOpacity>
+      <View style={{ flexDirection: 'row' }}>
+
+        <Text>Already Registered ? </Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+          <Text style={{ color: '#004dcf', fontWeight: 'bold' }}>Log in</Text>
+        </TouchableOpacity>
 
       </View>
     </SafeAreaView>
-    )
+  )
 }
 
 export default RegisterScreen;
