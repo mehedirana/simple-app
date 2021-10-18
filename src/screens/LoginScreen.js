2f3ea709b20be4a631f94c612a3304b99c613585
mehedirana <mdmehedirana@gmail.com>
2021-10-18 14:15:28 +0600

    master to mainssss

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -5,6 +5,8 @@ const LoginScreen =()=> {
     return (
         <View>
             <Text>hi I am log in</Text>
+            <Text>hi I am log in</Text>
+            <Text>hi I am log in</Text>
         </View>
     )
 }
