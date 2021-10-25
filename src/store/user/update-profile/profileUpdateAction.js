b93cfba4ebd38a1d2be4e788ac94fdf708920e61
mehedirana <mdmehedirana@gmail.com>
2021-10-25 21:34:28 +0600

    update profile add in redux

--- src/store/user/update-profile/profileUpdateAction.js
+++ src/store/user/update-profile/profileUpdateAction.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { userProfileURL } from "../../../utilites/proxyUrl"
-import { GET_ACCOUNT_DETAILS_FAIL, GET_ACCOUNT_DETAILS_REQUEST, GET_ACCOUNT_DETAILS_SUCCESS } from "../../actionTypes"
+import { ACCOUNT_UPDATE_FAIL, ACCOUNT_UPDATE_REQUEST, ACCOUNT_UPDATE_SUCCESS, GET_ACCOUNT_DETAILS_FAIL, GET_ACCOUNT_DETAILS_REQUEST, GET_ACCOUNT_DETAILS_SUCCESS } from "../../actionTypes"
 
 export const profile = (token) => async (dispatch) => {
     try {
@@ -22,5 +22,28 @@ export const profile = (token) => async (dispatch) => {
 
     } catch (error) {
         dispatch({ type: GET_ACCOUNT_DETAILS_FAIL, payload: error })
+    }
+}
+
+export const profileUpdate = (token, first_name, last_name) => async (dispatch) => {
+    try {
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`,
+                "Accept": "application/json",
+            },
+        };
+        dispatch({ type: ACCOUNT_UPDATE_REQUEST })
+
+        const res = await axios.post(userProfileURL, { first_name, last_name }, config)
+
+        if (!res || !res.data) {
+            dispatch({ type: ACCOUNT_UPDATE_FAIL, error: res.data })
+        }
+        dispatch({ type: ACCOUNT_UPDATE_SUCCESS, payload: res.data })
+
+    } catch (error) {
+        dispatch({ type: ACCOUNT_UPDATE_FAIL, payload: error })
     }
 }
--- src/store/user/update-profile/profileUpdateReducer.js
+++ src/store/user/update-profile/profileUpdateReducer.js
@@ -1,33 +1,41 @@
 import { ACCOUNT_UPDATE_FAIL, ACCOUNT_UPDATE_REQUEST, ACCOUNT_UPDATE_SUCCESS, GET_ACCOUNT_DETAILS_FAIL, GET_ACCOUNT_DETAILS_REQUEST, GET_ACCOUNT_DETAILS_SUCCESS } from "../../actionTypes"
 
-export const profileReducer =(state={}, action)=>{
+export const profileReducer = (state = {}, action) => {
     switch (action.type) {
-        case GET_ACCOUNT_DETAILS_REQUEST :
+        case GET_ACCOUNT_DETAILS_REQUEST:
             return {
-
+                success: false,
+                loading: true,
             }
-        case GET_ACCOUNT_DETAILS_SUCCESS :
+        case GET_ACCOUNT_DETAILS_SUCCESS:
             return {
-
+                success: true,
+                loading: false,
+                userProfile: action.payload
             }
-        case GET_ACCOUNT_DETAILS_FAIL :
+        case GET_ACCOUNT_DETAILS_FAIL:
             return {
-
+                success: false,
+                loading: false,
             }
-        case ACCOUNT_UPDATE_REQUEST :
+        case ACCOUNT_UPDATE_REQUEST:
             return {
-
+                success: false,
+                loading: true,
             }
-        case ACCOUNT_UPDATE_SUCCESS :
+        case ACCOUNT_UPDATE_SUCCESS:
             return {
-
+                success: true,
+                loading: false,
+                userProfile: action.payload,
             }
-        case ACCOUNT_UPDATE_FAIL :
+        case ACCOUNT_UPDATE_FAIL:
             return {
-
+                success: false,
+                loading: false,
             }
-    
-    
+
+
         default:
             state;
     }
